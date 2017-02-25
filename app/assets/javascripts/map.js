e3f7600a6a91648749d2bc110e21e69329aacfc6
佐藤有斗 <satoyuito@satoyuito-MacBook-Pro.local>
2017-02-25 20:34:57 +0900

    adjust map drawing

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,50 +1,59 @@
 $(function() {
-  console.log('hello');
-    var width = 600,
-    height = 500;
+  var width  = 900,
+      height = 1000;
 
-    function ajax(){
-      return $.ajax('/api/data.json', {
-        method: 'GET',
-        dataType: 'json',
-      });
-    }
-
-    ajax().done(function(result) {
-      console.log(result);
-    }).fail(function(result) {
-      console.log('error');
+  var svg = d3.select("body")
+    .append("svg")
+    .attr({
+      "width": width,
+      "height": height
     });
 
-    var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+  var color = d3.scale.category20();
 
-    var color = d3.scale.category20();
+  // 地理座標から画面表示への投影法の設定。
+  var mercator = d3.geo.mercator()
+    .center([136.0,35.6])
+    .translate([width/2, height/2])
+    .scale(1600);
 
-    // 地理座標から画面表示への投影法の設定。
-    var mercator = d3.geo.mercator()
-        .center([136.0,35.6])
-        .translate([width/2, height/2])
-        .scale(800);
-
-    // geojsonからpath要素を作るための設定。
-    var geopath = d3.geo.path()
+  // geojsonからpath要素を作るための設定。
+  var geopath = d3.geo.path()
     .projection(mercator);
 
-    // topojsonファイルの読み込み
-    d3.json("/assets/japan_topojson.json", function(error, jp) {
-        // console.log(jp);
+  console.log(geopath);
 
-        // topojsonからgeojsonへの変換。
-        var geoJp = topojson.feature(jp, jp.objects.ne_10m_admin_1_states_provinces);
-        console.log(geoJp);
+  // topojsonファイルの読み込み
+  d3.json("/assets/japan_topojson.json", function(error, jp) {
+    // console.log(jp);
 
-        svg.selectAll("path")
-            .data(geoJp.features) // geojsonのすべての県の座標データを読み込む。
-          .enter().append("path")
-            .attr("class", function(d) { return d.id; })
-            .attr("d", geopath) // geojsonからpath要素に変換する。
-            .attr("fill", function(d){ return color(d.geometry.coordinates); }); // idがないので、各県の座標リストに基づいて色を変える。
+    // topojsonからgeojsonへの変換。
+    var geoJp = topojson.feature(jp, jp.objects.ne_10m_admin_1_states_provinces);
+    console.log(geoJp);
+
+    svg.selectAll("path")
+      .data(geoJp.features) // geojsonのすべての県の座標データを読み込む。
+      .enter().append("path")
+      .attr("class", function(d) {
+        console.log(d);
+        return d.id;
+      })
+      .attr("d", geopath) // geojsonからpath要素に変換する。
+      .attr("fill", "#FFFFFF")
+      .attr("stroke", "#A9A9A9");
+
+      // idがないので、各県の座標リストに基づいて色を変える。
+  });
+
+  function ajax(){
+    return $.ajax('/api/data.json', {
+      method: 'GET',
+      dataType: 'json',
     });
+  }
+  ajax().done(function(result) {
+    console.log(result);
+  }).fail(function(result) {
+    console.log('error');
+  });
 });
