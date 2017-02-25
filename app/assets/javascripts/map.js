8cf8c8735b277263ffec1a9d68a173344a7ffe51
佐藤有斗 <satoyuito@satoyuito-MacBook-Pro.local>
2017-02-25 23:40:43 +0900

    Add test circles on map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -9,6 +9,22 @@ $(function() {
       "height": height
     });
 
+  var circle = d3.select("body")
+    .append("svg")
+    .attr({
+      "width": width,
+      "height": height
+    });
+
+  circle.append("circle")
+    .attr("cx",500)
+    .attr("cy",-300)
+    .attr("r",20)
+    .attr("fill","green")
+    .attr("stroke-width",3)
+    .attr("stroke","orange");
+
+
   var color = d3.scale.category20();
 
   // 地理座標から画面表示への投影法の設定。
@@ -21,28 +37,69 @@ $(function() {
   var geopath = d3.geo.path()
     .projection(mercator);
 
-  console.log(geopath);
-
   // topojsonファイルの読み込み
+
+  var g = svg.append("g");
+
   d3.json("/assets/japan_topojson.json", function(error, jp) {
     // console.log(jp);
 
+
+
     // topojsonからgeojsonへの変換。
     var geoJp = topojson.feature(jp, jp.objects.ne_10m_admin_1_states_provinces);
-    console.log(geoJp);
+    // console.log(geoJp.features);
 
-    svg.selectAll("path")
-      .data(geoJp.features) // geojsonのすべての県の座標データを読み込む。
-      .enter().append("path")
-      .attr("class", function(d) {
-        console.log(d);
-        return d.id;
-      })
-      .attr("d", geopath) // geojsonからpath要素に変換する。
-      .attr("fill", "#FFFFFF")
-      .attr("stroke", "#A9A9A9");
+    var positions = [];
 
-      // idがないので、各県の座標リストに基づいて色を変える。
+    g.append("g")
+      .selectAll("path")
+        .data(geoJp.features) // geojsonのすべての県の座標データを読み込む。
+        .enter().append("path")
+        .attr("class", function(d) {
+          return d.id;
+        })
+        .attr("d", geopath)
+        .attr("centroid", function(d) {
+          var centroid = geopath.centroid(d);
+          positions.push(centroid);
+        }) // geojsonからpath要素に変換する。
+        .attr("fill", "#FFFFFF")
+        .attr("stroke", "#A9A9A9")
+        // idがないので、各県の座標リストに基づいて色を変える。
+
+    for (var i = 0; i < positions.length; i ++) {
+      g.append("circle")
+        .style("fill", "red")
+        .attr("transform", function(d) {
+          var centroid = positions[i];
+          console.log(centroid);
+          return "translate(" + centroid +")";
+        })
+        .attr({
+          "cx": 0,
+          "cy": 0,
+          "r": 7
+        });
+    }
+
+    for (var i = 0; i < positions.length; i ++) {
+      g.append("circle")
+        .style("fill", "blue")
+        .attr("transform", function(d) {
+          var centroid = [positions[i][0] + 7, positions[i][1] + 7];
+          console.log(centroid);
+          return "translate(" + centroid +")";
+        })
+        .attr({
+          "cx": 0,
+          "cy": 0,
+          "r": 7
+        });
+    }
+
+    console.log(positions);
+
   });
 
   function ajax(){
