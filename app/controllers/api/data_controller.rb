abaff84bd1f6ec054e910f6350760fdac633d981
佐藤有斗 <satoyuito@satoyuito-MacBook-Pro.local>
2017-02-26 01:18:36 +0900

    Fix data

--- app/controllers/api/data_controller.rb
+++ app/controllers/api/data_controller.rb
@@ -1,5 +1,10 @@
 class Api::DataController < ApplicationController
   def index
-    @data = Datum.all
+    @data = Datum.all.group(:area, :middle_category).count.reduce({}) do |result, (key, value)|
+      area, middle_category = key
+      result[area] ||= {}
+      result[area][middle_category] = value
+      result
+    end
   end
 end
