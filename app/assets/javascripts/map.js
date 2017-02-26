9c9d9c48e92a1e3bc263e0576f5d05757da2a8aa
佐藤有斗 <satoyuito@satoyuito-MacBook-Pro.local>
2017-02-26 10:11:45 +0900

    Add centroids in regions

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -40,48 +40,96 @@ $(function() {
         .attr("centroid", function(d) {
           var centroid = geopath.centroid(d);
           centroids.push(centroid);
-        }) // geojsonからpath要素に変換する。
+        })
+
+         // geojsonからpath要素に変換する。
         .attr("fill", "#FFFFFF")
         .attr("stroke", "#A9A9A9")
 
-    for (var i = 0; i < centroids.length; i ++) {
+    var region_ids = [3, 8, 12, 16, 22, 26, 33, 43];
+    var region_centroids = [];
+
+    for (var i = 0; i < region_ids.length; i ++) {
+      region_centroids.push(centroids[region_ids[i]])
+      // debugger
+    }
+    console.log(centroids)
+    console.log(region_ids)
+    console.log(region_centroids)
+
+    // for (var i = 0; i < centroids.length; i ++) {
+    //   // console.log(region_centroids[i])
+    //   g.append("circle")
+    //     .style("fill", "blue")
+    //     .attr("transform", function(d) {
+    //       var centroid = centroids[i];
+    //       return "translate(" + centroid +")";
+    //     })
+    //     .attr({
+    //       "id": i,
+    //       "cx": 0,
+    //       "cy": 0,
+    //       "r": 2
+    //     });
+    // }
+
+
+    for (var i = 0; i < region_centroids.length; i ++) {
+      // console.log(region_centroids[i])
       g.append("circle")
         .style("fill", "red")
         .attr("transform", function(d) {
-          var centroid = centroids[i];
+          var centroid = region_centroids[i];
           return "translate(" + centroid +")";
         })
         .attr({
+          "id": i,
           "cx": 0,
           "cy": 0,
-          "r": 7
+          "r": 3
         });
     }
 
-    for (var i = 0; i < centroids.length; i ++) {
-      g.append("circle")
-        .style("fill", "blue")
-        .attr("transform", function(d) {
-          var centroid = [centroids[i][0] + 7, centroids[i][1] + 7];
-          return "translate(" + centroid +")";
-        })
-        .attr({
-          "cx": 0,
-          "cy": 0,
-          "r": 7
-        });
-    }
+    g.append("circle")
+      .style("fill", "red")
+      .attr("transform", function(d) {
+        return "translate(" + centroids[22] +")";
+      })
+      .attr({
+        "id": i,
+        "cx": 0,
+        "cy": 0,
+        "r": 3
+      });
+
+    // for (var i = 0; i < centroids.length; i ++) {
+    //   g.append("circle")
+    //     .style("fill", "blue")
+    //     .attr("transform", function(d) {
+    //       var centroid = [centroids[i][0] + 7, centroids[i][1] + 7];
+    //       return "translate(" + centroid +")";
+    //     })
+    //     .attr({
+    //       "cx": 0,
+    //       "cy": 0,
+    //       "r": 7
+    //     });
+    // }
   });
 
-  function ajax(){
-    return $.ajax('/api/data.json', {
-      method: 'GET',
-      dataType: 'json',
-    });
-  }
-  ajax().done(function(result) {
-    console.log(result);
-  }).fail(function(result) {
-    console.log('error');
+  // function ajax(){
+  //   return $.ajax('/api/data.json', {
+  //     method: 'GET',
+  //     dataType: 'json',
+  //   });
+  // }
+  // ajax().done(function(result) {
+  //   console.log(result);
+  // }).fail(function(result) {
+  //   console.log('error');
+  // });
+
+  $.getJSON("/assets/data.json", function(data) {
+    console.log(data);
   });
 });
